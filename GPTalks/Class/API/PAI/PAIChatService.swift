9f1a55809caa8f01f70d444443c6263cd19ab52c
Zabir Raihan <zabirraihan6@gmail.com>
2023-11-10 23:02:08 +0600

    Pawan AI provider, diff bubble color for diff provider

---
+++ GPTalks/Class/API/PAI/PAIChatService.swift
@@ -0,0 +1,28 @@
+//
+//  Pawan.swift
+//  GPTalks
+//
+//  Created by Zabir Raihan on 10/11/2023.
+//
+
+import SwiftUI
+
+class PawanService: BaseChatService {
+    
+    override var baseURL: String {
+        return "https://api.pawan.krd"
+    }
+    
+    override var path: String {
+        return "/v1/chat/completions"
+    }
+    
+    override var headers: [String: String] {
+        [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer \(AppConfiguration.shared.PAIkey)",
+        ]
+    }
+    
+}
+
---
+++ GPTalks/Class/View/Setting/PAISettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  PAISettingsView.swift
+//  GPTalks
+//
+//  Created by Zabir Raihan on 10/11/2023.
+//
+
+import SwiftUI
+
+struct PAISettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PAISettingsView()
+}
