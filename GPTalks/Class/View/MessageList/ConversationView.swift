087868abeeed14af54d83fe0dc7030c39b9e49ae
Zabir Raihan <zabirraihan6@gmail.com>
2023-11-13 03:55:32 +0600

    cleanup context menu button

--- GPTalks/Class/View/MessageList/ConversationView.swift
+++ GPTalks/Class/View/MessageList/ConversationView.swift
@@ -6,53 +6,50 @@
 //
 
 import SwiftUI
-import SwiftUIX
+// import SwiftUIX
 
 struct ConversationView: View {
-    
     let conversation: Conversation
     let accentColor: Color
-    
+
     let regenHandler: (Conversation) -> Void
     let editHandler: (Conversation) -> Void
-    
+
     @State var isEditing: Bool = false
     @FocusState var isFocused: Bool
     @State var editingMessage: String = ""
     var deleteHandler: (() -> Void)?
-    
+
     @State private var isHovered = false
-    
+
     var body: some View {
-        VStack(spacing: 1) {
+        VStack {
             if conversation.role == "user" {
                 userMessage
-//                    .onHover { hover in
-//                        self.isHovered = hover
-//                    }
-
+                    .padding(.trailing, 15)
+                    .padding(.leading, horizontalPadding)
             } else if conversation.role == "assistant" {
                 assistantMessage
-//                    .onHover { hover in
-//                        self.isHovered = hover
-//                    }
-
-
+                    .padding(.leading, 15)
+                    .padding(.trailing, horizontalPadding)
             } else {
                 ReplyingIndicatorView()
-                
             }
         }
-        #if os(iOS)
-        .contextMenu {
-            contextMenu
+        .onHover { hover in
+            self.isHovered = hover
         }
+        .padding(.vertical, 5)
+        #if os(iOS)
+            .contextMenu {
+                contextMenu
+            }
         #endif
     }
-    
+
     @ViewBuilder
     private var userMessage: some View {
-        HStack(spacing: 0) {
+        HStack(alignment: .lastTextBaseline, spacing: 0) {
             Spacer()
             if isEditing {
                 editControls()
@@ -61,69 +58,49 @@ struct ConversationView: View {
                     .textFieldStyle(.plain)
                     .bubbleStyle(isMyMessage: true, type: .textEdit)
             } else {
-                Menu {
-                   contextMenu
-                } label: {
-                    Label("Options", systemImage: "ellipsis.circle")
-                        .labelStyle(.iconOnly)
-                }
-                
-                .menuIndicator(.hidden)
-                .menuStyle(.borderlessButton)
-                .frame(width: 20, height: 20)
-                .visible(isHovered)
-                
+                optionsMenu()
                 Text(conversation.content)
                     .textSelection(.enabled)
                     .bubbleStyle(isMyMessage: true, type: .text, accentColor: accentColor)
             }
         }
-        .padding(.leading, horizontalPadding)
-        .padding(.vertical, 5)
-        .padding(.trailing, 15)
-        .onHover { hover in
-            self.isHovered = hover
-        }
     }
-    
+
     @ViewBuilder
     private var assistantMessage: some View {
-        HStack(spacing: 2) {
+        HStack(alignment: .lastTextBaseline, spacing: 4) {
             VStack(alignment: .leading) {
-                if AppConfiguration.shared.isMarkdownEnabled{
+                if AppConfiguration.shared.isMarkdownEnabled {
                     MessageMarkdownView(text: conversation.content)
                         .textSelection(.enabled)
                 }
-                
                 if conversation.isReplying {
                     ReplyingIndicatorView()
                         .frame(width: 48, height: 16)
                 }
             }
             .bubbleStyle(isMyMessage: false, type: .text)
-            
-            Menu {
-               contextMenu
-            } label: {
-                Label("Options", systemImage: "ellipsis.circle")
-                    .labelStyle(.iconOnly)
+            if !conversation.isReplying {
+                optionsMenu()
             }
-            .menuIndicator(.hidden)
-            .menuStyle(.borderlessButton)
-            .frame(width: 20, height: 20)
-            .visible(isHovered)
-            
             Spacer()
         }
-        .padding(.trailing, horizontalPadding)
-        .padding(.vertical, 5)
-        .padding(.leading, 15)
-        .onHover { hover in
-            self.isHovered = hover
-        }
     }
-    
-    
+
+    @ViewBuilder
+    private func optionsMenu() -> some View {
+        Menu {
+            contextMenu
+        } label: {
+            Label("Options", systemImage: "ellipsis.circle")
+                .labelStyle(.iconOnly)
+        }
+        .menuIndicator(.hidden)
+        .menuStyle(.borderlessButton)
+        .frame(width: 20, height: 20)
+        .visible(isHovered)
+    }
+
     @ViewBuilder
     func editControls() -> some View {
         HStack(spacing: 15) {
@@ -134,7 +111,7 @@ struct ConversationView: View {
             }
             .buttonStyle(.borderless)
             .foregroundColor(.red)
-            
+
             Button {
                 editHandler(Conversation(role: "user", content: editingMessage))
                 isEditing = false
@@ -145,11 +122,10 @@ struct ConversationView: View {
             .buttonStyle(.borderless)
             .foregroundColor(.green)
             .keyboardShortcut(isEditing ? .defaultAction : .none)
-            
         }
         .padding(.trailing, 10)
     }
-    
+
     @ViewBuilder
     private var contextMenu: some View {
         if conversation.role == "assistant" {
@@ -191,12 +167,12 @@ struct ConversationView: View {
             }
         }
     }
-    
+
     private var horizontalPadding: CGFloat {
-#if os(iOS)
-        return 55
-#else
-        return 95
-#endif
+        #if os(iOS)
+            return 55
+        #else
+            return 95
+        #endif
     }
 }
