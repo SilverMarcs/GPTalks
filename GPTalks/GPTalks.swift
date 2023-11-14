22c914a2bc18c4977c4b6c6f50cd96d3befa8f4d
Zabir Raihan <zabirraihan6@gmail.com>
2023-11-14 21:33:14 +0600

    bugfix cannot enter openai key on launch

--- GPTalks/GPTalks.swift
+++ GPTalks/GPTalks.swift
@@ -25,7 +25,7 @@ struct GPTalks: App {
                 .alert("Enter OpenAI API Key", isPresented: $showOpenAIKeyAlert) {
                     TextField("OpenAI API Key", text: AppConfiguration.shared.$OAIkey)
                     Button("Later", role: .cancel) { }
-                    Button("Confirm", role: .none) { }.disabled(AppConfiguration.shared.OAIkey.isEmpty)
+                    Button("Confirm", role: .none) { }
                 } message: {
                     Text("You need set OpenAI API Key before start a conversation.")
                 }
