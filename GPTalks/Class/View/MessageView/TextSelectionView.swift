5c0405b7a0adb2673979670591ada8c93a90605b
Zabir Raihan <zabirraihan6@gmail.com>
2024-01-14 12:53:03 +0800

    select individual text in ios remove chat bubbble accnt color

---
+++ GPTalks/Class/View/MessageView/TextSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  TextSelectionView.swift
+//  GPTalks
+//
+//  Created by Zabir Raihan on 14/01/2024.
+//
+
+import SwiftUI
+
+struct TextSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextSelectionView()
+}
