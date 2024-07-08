259d2b9c8c6d29b0915ad8f66b3c42409ebd695b
Zabir Raihan <zabirraihan6@gmail.com>
2024-07-09 00:52:51 +0600

    enhance provider detail general view

--- GPTalks/View/SettingsViews/ProviderViews/ProviderGeneral.swift
+++ GPTalks/View/SettingsViews/ProviderViews/ProviderGeneral.swift
@@ -16,8 +16,11 @@ struct ProviderGeneral: View {
 
     var body: some View {
         Form {
+            Section {
+                header
+            }
+            
             Section("Host Settings") {
-                TextField("Name", text: $provider.name)
                 if provider.type == .google {
                     TextField("Host URL", text: .constant("generativelanguage.googleapis.com"))
                         .disabled(true)
@@ -25,21 +28,6 @@ struct ProviderGeneral: View {
                     TextField("Host URL", text: $provider.host)
                 }
                 SecureField("API Key", text: $provider.apiKey)
-            }
-
-            if provider.id.uuidString == providerManager.defaultProvider {
-                Text("DEFAULT")
-                    .font(.body)
-                    .foregroundStyle(.secondary)
-
-            } else {
-                Button {
-                    providerManager.defaultProvider = provider.id.uuidString
-                } label: {
-                    Text("Set as Default")
-                }
-                .buttonStyle(.plain)
-                .foregroundStyle(.blue)
             }
 
             Section("Customisation") {
@@ -60,12 +48,50 @@ struct ProviderGeneral: View {
         }
         .formStyle(.grouped)
     }
+    
+    private var header: some View {
+        HStack {
+            ProviderImage(color: Color(hex: provider.color))
+
+            TextEditor(text: $provider.name)
+                .textEditorStyle(.plain)
+                .font(.title)
+                .padding(5)
+                .onChange(of: provider.name) {
+                    provider.name = String(provider.name.trimmingCharacters(in: .whitespacesAndNewlines).prefix(18))
+                }
+            
+            Spacer()
+            
+            defaultSetter
+        }
+    }
+    
+    private var defaultSetter: some View {
+        Group {
+            if provider.id.uuidString == providerManager.defaultProvider {
+                Text("DEFAULT")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
+                
+            } else {
+                Button {
+                    providerManager.defaultProvider = provider.id.uuidString
+                } label: {
+                    Text("Set as Default")
+                }
+                .buttonStyle(.plain)
+                .foregroundStyle(.blue)
+            }
+        }
+    }
 }
 
 #Preview {
     let provider = Provider.getDemoProvider()
+    provider.type = .google
 
-    ProviderGeneral(provider: provider)
+    return ProviderGeneral(provider: provider)
         .padding()
         .frame(width: 500)
 }
