ea09927cc902783298b0c7cda4ed6bbbd3940fad
Zabir Raihan <zabirraihan6@gmail.com>
2024-08-19 15:14:22 +0800

    progressview hwne markdown is loading

--- GPTalks/MarkdownViews/MarkdownView.swift
+++ GPTalks/MarkdownViews/MarkdownView.swift
@@ -16,14 +16,23 @@ struct MarkdownView: View {
         conversation.group?.session?.searchText.count ?? 0 > 3 ? conversation.group?.session?.searchText : nil
     }
     
+    @State var isRendered = false
+    
     var body: some View {
         switch config.markdownProvider {
             case .webview:
+            if !isRendered {
+                ProgressView()
+            }
+            
             MarkdownWebView(conversation.content,
                             baseURL: "GPTalks Web Content",
                             highlightString: highlightString,
                             customStylesheet: config.markdownTheme,
                             fontSize: CGFloat(config.fontSize))
+            .onRendered { content in
+                isRendered = true
+            }
             case .native:
                 Text(LocalizedStringKey(conversation.content))
                 .font(.system(size: config.fontSize))
